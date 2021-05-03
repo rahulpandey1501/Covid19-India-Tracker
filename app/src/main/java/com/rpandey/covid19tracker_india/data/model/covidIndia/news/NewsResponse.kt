9a3f8f872cb44c1cd32bfbd748af0e1c7dd14032
Rahul Pandey <rahul.pandey@RahulPandey.local>
2021-05-03 23:42:07 +0530

    upgrade gralde and news fix

--- app/src/main/java/com/rpandey/covid19tracker_india/data/model/covidIndia/news/NewsResponse.kt
+++ app/src/main/java/com/rpandey/covid19tracker_india/data/model/covidIndia/news/NewsResponse.kt
@@ -4,9 +4,15 @@ import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 
 @Keep
-data class NewsResponse(
-    @SerializedName("source") val source: String?,
-    @SerializedName("headlines") val headlines: List<String>,
-    @SerializedName("headlines_summary") val summary: List<String>?,
-    @SerializedName("image_link") val images: List<String>?
+data class NewsListResponse constructor(
+    @SerializedName("category") val category: String,
+    @SerializedName("data") val data: List<NewsData>?,
+    @SerializedName("success") val success: Boolean
+)
+@Keep
+data class NewsData constructor(
+    @SerializedName("title") val title: String?,
+    @SerializedName("content") val content: String?,
+    @SerializedName("imageUrl") val imageUrl: String?,
+    @SerializedName("readMoreUrl") val readMoreUrl: String?
 )
